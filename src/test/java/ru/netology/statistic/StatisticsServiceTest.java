ca51d6ba6961d51ece388c9a14132ce5ffc05bc9
Fedor Sharov <1710193@mail.ru>
2022-07-11 20:36:53 +0300

    Syndrome 100% Completed

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxStartWithMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxFromNegative() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -101};
+        long expected = -3;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
